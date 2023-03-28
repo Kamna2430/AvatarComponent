f68be8e97e524a84b8268f8e89f135633b50eb99
Kamna Raj -X (kamnraj - ARICENT TECHNOLOGIES HOLDINGS LIMITED at Cisco) <krlink.code@gmail.com>
2023-03-28 17:03:04 +0530

    Component

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Avatar } from "@momentum-ui/react";
+
+const App = () => {
+  return (
+    <div>
+      <Avatar
+        color="default"
+        initials="AS"
+        presence="default"
+        size="xsmall"
+        src="https://images.unsplash.com/photo-1583195764036-6dc248ac07d9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2855&q=80"
+        title="Cisco Webex"
+        type="person"
+      />
+      ;
+    </div>
+  );
+};
+
+export default App;
